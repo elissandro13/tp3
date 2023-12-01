cfc13fa76530af5a3bddd648dcb37fe6bd8fd8f8
elissndro13 <elissandro13@github.com>
2023-12-01 12:59:43 -0300

    Add util.hpp

---
+++ include/util.hpp
@@ -0,0 +1,18 @@
+//
+// Created by ecjun on 01/12/2023.
+//
+
+#ifndef TP3_UTIL_HPP
+#define TP3_UTIL_HPP
+
+#include "matrix.hpp"
+
+
+    Matrix multiplicaMatrix(Matrix a, Matrix b);
+
+    Vetor multiplicaMatrixVetor(Matrix a, int x, int y);
+
+
+
+
+#endif //TP3_UTIL_HPP
